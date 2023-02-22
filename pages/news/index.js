4653d3b53552d5da9e1d83327570dd1a2c09acee
couponthemes <devs@jessoreit.com>
2023-02-22 18:10:44 +0600

    first commit

---
+++ pages/news/index.js
@@ -0,0 +1,30 @@
+function NewsArticleList({ articles }) {
+  return (
+    <>
+      <h1>List of News</h1>
+      {articles.map((article) => {
+        return (
+          <div key={article.id}>
+            <h4>
+              {article.id} - {article.title}
+            </h4>
+            <p>Category: {article.category}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default NewsArticleList;
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:4000/news");
+  const data = await res.json();
+
+  return {
+    props: {
+      articles: data,
+    },
+  };
+}
